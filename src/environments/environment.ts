2612d1fca3e4e30cb2ca100a3ee4f2e3d9f7365e
Samir El Hassan <samirelhassann@gmail.com>
2025-03-11 23:04:42 -0300

    ci: configure github-pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
-import { secrets } from "./secrets";
 
 export const environment = {
   production: false,
 
   github: {
     host: "https://api.github.com",
-    token: secrets.API_TOKEN,
+    token: "",
   },
 };
